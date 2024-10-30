3c03864a84c6a11f1a7c72652cb465d1075fdbbb
gonzaloseg <157105370+gonzaloseg@users.noreply.github.com>
2024-10-30 14:01:32 +0100

    Se agrega el proyecto

---
+++ src/main/java/ConVive/ConVive/App.java
@@ -0,0 +1,13 @@
+package ConVive.ConVive;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
