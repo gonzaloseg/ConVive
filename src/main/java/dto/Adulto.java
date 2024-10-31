700a4a5bb6dd34e05dd8792011e0d730275346ee
Belén Márquez <belen.marquezlopez05@gmail.com>
2024-10-31 13:25:13 +0100

    prueba desde eclipse

--- src/main/java/dto/Adulto.java
+++ src/main/java/dto/Adulto.java
@@ -1,5 +1,12 @@
 package dto;
 
 public class Adulto {
+	
+	private int id;
+	private String dni, nombre, apellidos;
+//	private date j;
+	private String contrasenia, piso;
+	
+	//hola estoy probando desde eclipse github
 
 }
