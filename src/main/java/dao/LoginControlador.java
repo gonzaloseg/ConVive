997f39d1f38c3a6d3970a6207d798b95b3ea5db9
Belén Márquez <belen.marquezlopez05@gmail.com>
2024-11-06 13:21:23 +0100

    comboBox Viviendas relleno

--- src/main/java/dao/LoginControlador.java
+++ src/main/java/dao/LoginControlador.java
@@ -1,8 +1,11 @@
 package dao;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -10,6 +13,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.fxml.Initializable;
 
@@ -25,7 +29,9 @@ public class LoginControlador implements Initializable{
     @FXML
     private TextField txtnombre;
     @FXML
-    private ComboBox<?> comboboxVivienda;
+    private ComboBox<String> comboboxVivienda;
+    @FXML
+    private VBox vboxPisos;
     @FXML
     private DatePicker datepickerFechaN;
     @FXML
@@ -34,26 +40,23 @@ public class LoginControlador implements Initializable{
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
-		// Texto que aparecerá en los TextFields
+		//Texto que aparecerá en los TextFields
         txtDNI.setPromptText("Ingrese su DNI");
         txtcontrasenia.setPromptText("Ecriba una  contraseña");
         txtcontrasenia2.setPromptText("Repita su contraseña");
         txtnombre.setPromptText("Ecriba su nombre");
         txtapellidos.setPromptText("Ecriba su nombre");
 		
+        //Para rellenar el combobox con cada piso
+        ObservableList<String> pisos = FXCollections.observableArrayList();
+        for (int i = 1; i <= 7; i++) {
+            pisos.add(i + "ºA"); //añade los pisos A
+            pisos.add(i + "ºB"); //añade los pisos B
+        }
+        comboboxVivienda.setItems(pisos); //carga los pisos al combobox
 	}
 	
-	 @FXML
-    void seleccionarVivienda(ActionEvent event) {
-/*
- 		Producto productoSeleccionado = sbComboBox.getSelectionModel().getSelectedItem();
-        if (productoSeleccionado != null) {
-            labelResultado.setText("Producto seleccionado: " + productoSeleccionado.getNombre());
-        } else {
-            labelResultado.setText("No ha seleccionado ningún producto.");
-        }
- */
-    }
+	 
 	
 	@FXML
     void volver(ActionEvent event) { //BOTON VOLVER 
